e1844323a442de43b8faa2864cfb645f394b29e7
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-17 10:24:00 +0530

    [add] Shubham added UC2 of EmpPayrollApp-AddEmpToLocalStorage

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -45,7 +45,12 @@ class EmployeePayroll {
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
-        this._startDate = startDate;
+        let now = new Date();
+        if (startDate > now) throw 'Start Date is a Future Date!';
+        var diff = Math.abs(now.getTime() - startDate.getTime());
+        if (diff / (1000 * 60 * 60 * 24) > 30) 
+          throw 'Start Date is beyond 30 Days!';
+        this._startDate = startDate; 
     }
     // toString() method
     toString() {
